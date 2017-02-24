9fd352d5bf120dec5021c5d3b16265baa0447e53
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2017-02-24 17:34:24 -0500

    adding test for ifort and impi
    Change-Id: I0e6a27dc997962f48b1a8c91f7224bc2e1a399da

---
+++ impi/2017.1.132/mpiicc.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+module purge
+module load iccifort/2017.1.132-GCC-5.2.0
+module=impi
+version=2017.1.132
+module load $module/$version
+
+if [ $? != 0 ]; then
+	echo "unable to load module $module/$version"
+	exit 1
+fi
+
+mpiicc -v
+if [ $? != 0 ]; then
+	echo "Unable to run mpiicc -v"
+	exit 1
+fi
+
---
+++ impi/2017.1.132/testall.sh
@@ -0,0 +1,8 @@
+sh mpiicc.sh 
+sh mpiifort.sh 
+sh mpiicpc.sh 
+sh mpif77.sh 
+sh mpif90.sh 
+sh mpicc.sh 
+sh mpicxx.sh 
+sh cpuinfo.sh 
