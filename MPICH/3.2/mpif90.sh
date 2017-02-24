2cb7aae3163beda7cec0771889c1e885ed53a3c5
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2017-02-24 17:23:53 -0500

    adding MPICH test for GCC 5.2.0
    Change-Id: Ibc130c7dd855005d1139dbaee344d4c9931219a6

---
+++ MPICH/3.2/mpif90.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+module purge
+module load GCC/5.2.0
+module=MPICH
+version=3.2
+module load $module/$version
+
+if [ $? != 0 ]; then
+	echo "unable to load module $module/$version"
+	exit 1
+fi
+
+mpif90 -v
+if [ $? != 0 ]; then
+	echo "Unable to run mpif90 -v"
+	exit 1
+fi
+
---
+++ MPICH/3.2/testall.sh
@@ -0,0 +1,6 @@
+sh mpicc.sh 
+sh mpic++.sh 
+sh mpif90.sh 
+sh helloworld_c.sh 
+sh helloworld_cpp.sh 
+sh helloworld_fortran.sh 
