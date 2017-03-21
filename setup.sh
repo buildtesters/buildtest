933353766196728cfbb08ed7bc543eebfb3bd7a3
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2017-03-21 11:00:48 -0400

    Fixed the software,version check verification into a 2-step validation process.   1. Check if module file exist for software and version to be tested. Requires string literal equality especially when module file is hidden file   2. Check easyconfig toolchain field to make sure toolchain tag matches the toolchain provided by command line. If not found, then halt program
    Also added check if there is no test directory in src or if there are no test files in the directory
    I clone my easyconfig repo in this repo and add path for BUILDTEST_EASYCONFIGDIR in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-export BTROOT=`echo $PWD`
-export BTMODROOT=/nfs/grid/software/testing/RHEL7/easybuild/modules/
-export BTTEMPLATEDIR=$BTROOT/template
-export BTSRCDIR=$BTROOT/src
+export BUILDTEST_ROOT=`echo $PWD`
+export BUILDTEST_MODROOT=/nfs/grid/software/testing/RHEL7/easybuild/modules/
+export BUILDTEST_TEMPLATEDIR=$BUILDTEST_ROOT/template
+export BUILDTEST_SRCDIR=$BUILDTEST_ROOT/src
+export BUILDTEST_EASYCONFIGDIR=$BUILDTEST_ROOT/easybuild
