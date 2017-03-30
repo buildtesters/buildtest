3f93e9aa212404834dbed783c1b8403f198b8ce8
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2017-03-30 14:53:10 -0400

    deleting BuildTest Shell-script implementation and all necessary files

--- setup.sh
+++
@@ -1,7 +0,0 @@
-export BUILDTEST_ROOT=`echo $PWD`
-export BUILDTEST_TEMPLATEDIR=$BUILDTEST_ROOT/template
-export BUILDTEST_SRCDIR=$BUILDTEST_ROOT/src
-
-# site-specific: specify the parameters below accordingly 
-export BUILDTEST_EASYCONFIGDIR=$BUILDTEST_ROOT/easybuild
-export BUILDTEST_MODROOT=/nfs/grid/software/testing/RHEL7/easybuild/modules/
--- src/intel/mpi/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-export CC=mpiicc
-export CXX=mpiicpc
-export FC=mpiifort
-export F77=mpiifort
-export F90=mpiifort
