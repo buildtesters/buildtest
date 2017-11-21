4ab8870eddb9da5177b6c414e98f1231d14b35ab
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2017-11-21 15:51:13 -0500

    adding feature --sysyaml for generating YAML configuration for binary test for system package. Checks whether system package is installed and ensures it doesn't override existing command.yaml file.

---
+++ framework/tools/generate_yaml.py
@@ -0,0 +1,72 @@
+############################################################################
+#
+#  Copyright 2017
+#
+#   https://github.com/HPC-buildtest/buildtest-framework
+#
+#  This file is part of buildtest.
+#
+#    buildtest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    buildtest is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with buildtest.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+
+"""
+This module generates yaml configuration files 
+
+:author: Shahzeb Siddiqui (Pfizer)
+"""
+
+import os
+import sys
+import yaml
+from framework.env import BUILDTEST_SOURCEDIR, BUILDTEST_MODULE_EBROOT
+from framework.tools.software import get_unique_software
+from framework.tools.system import check_system_package_installed, get_binaries_from_systempackage
+def create_system_yaml(name):
+	""" create YAML configuration for binary test for system package """
+
+	softwarelist = get_unique_software(BUILDTEST_MODULE_EBROOT)
+	destdir = os.path.join(BUILDTEST_SOURCEDIR,"system",name)
+	yamlfile = os.path.join(destdir,"command.yaml")
+
+	# if yaml file exists then exit out
+	if os.path.isfile(yamlfile):
+		print "YAML file already exists, please check: ", yamlfile
+		sys.exit(0)
+	
+	# if directory is not present then create it
+	if not os.path.isdir(destdir):
+		print "creating directory ", destdir
+		os.makedirs(destdir)
+
+	# if package is not installed
+	if check_system_package_installed(name) == False:
+		print "Please install system package:", name, " before creating YAML file"
+		sys.exit(0)
+
+	# get binary from system package
+	binary = get_binaries_from_systempackage(name)
+
+	# no test, then stop immediately
+	if len(binary) == 0:
+		print "There are no binaries found in package: ", name
+		sys.exit(0)
+
+	fd = open(yamlfile,"w")
+	binarydict = { "binaries": binary }
+	with open(yamlfile, 'a') as outfile:
+		yaml.dump(binarydict, outfile, default_flow_style=False)
+
+	print "Please check YAML file", yamlfile, " and fix test accordingly"
+	sys.exit(0)
+	
---
+++ framework/tools/system.py
@@ -0,0 +1,71 @@
+############################################################################
+#
+#  Copyright 2017
+#
+#   https://github.com/HPC-buildtest/buildtest-framework
+#
+#  This file is part of buildtest.
+#
+#    buildtest is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    buildtest is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with buildtest.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+
+"""
+:author: Shahzeb Siddiqui (Pfizer)
+
+Functions for system package 
+"""
+import os
+import stat
+from stat import S_IXUSR, S_IXGRP, S_IXOTH
+import subprocess
+
+def check_system_package_installed(pkg):
+	""" check if system package is installed and return True/False"""
+
+	cmd = "rpm -q " + pkg
+        ret = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        ret.communicate()
+
+	if ret.returncode == 0:
+		return True
+	else:
+		return False
+	
+def get_binaries_from_systempackage(pkg):
+	""" get binaries from system package that typically install in standard linux path and only those that are executable """
+
+	bindirs = [ "/usr/bin", "/bin", "/sbin", "/usr/sbin", "/usr/local/bin", "/usr/local/sbin" ]
+
+	cmd = "rpm -ql " + pkg  
+        ret = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+        (output,err) = ret.communicate()
+	
+	temp = output.splitlines()
+	output = temp
+	
+	binarylist = []
+
+	for file in output:
+		# if file doesn't exist but found during rpm -ql then skip file. 
+		if not os.path.isfile(file):
+			continue
+
+		# check only files that are executable
+		statmode = os.stat(file)[stat.ST_MODE] & (stat.S_IXUSR|stat.S_IXGRP|stat.S_IXOTH)
+
+		# only add executable files found in array bindirs
+		if statmode and os.path.dirname(file) in bindirs:
+			binarylist.append(file)
+
+	return binarylist
