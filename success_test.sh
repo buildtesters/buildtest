19dd31154d3ac0574cc39744b7ecef867d58dff8
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2018-08-06 17:34:49 -0400

    Fix a bug  with --sysyaml and now the behavior is similar to --ebyaml where it checks for sha256sum before writing content

--- success_test.sh
+++ success_test.sh
@@ -1,13 +1,11 @@
 buildtest -s GCC/5.4.0-2.27 --shell csh
-buildtest -s OpenMPI/2.0.0 -t GCC/5.4.0-2.27 --testset MPI --job-template template/job.lsf
-buildtest -s Python/2.7.12 -t intel/2017.01 --testset Python
-buildtest -s R/3.3.1 -t intel/2017.01 --testset R
-buildtest --system all
-buildtest --system gcc
-buildtest -s intel/2017.01 
-buildtest -s Ruby/2.3.4 --testset Ruby
-buildtest -s Perl/5.22.1 -t foss/.2016.03 --testset Perl
-buildtest -s Tcl/.8.6.5 -t intel/2017.01
-buildtest -s Tcl/.8.6.5 -t intel/2017.01 --testset Tcl
+_buildtest --system all
+_buildtest --system gcc
+_buildtest -s intel/2017.01 
+_buildtest -s Python/2.7.14-intel-2018a --python-package dateutil
+_buildtest -s R/3.4.3-intel-2018a-X11-20171023 --r-package abc
+_buildtest -s Ruby/2.5.0-intel-2018a --ruby-package addressable
+ _buildtest -s Perl/5.26.0-GCCcore-6.4.0 --perl-package AnyData
+_buildtest -s Tcl/.8.6.5 -t intel/2017.01
 
 
