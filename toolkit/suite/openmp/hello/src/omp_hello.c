8f3f29cd332d1e8ae7eea3ff6275418eb64a6346
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2019-10-26 21:19:18 -0400

    add license tag to source code for tests. removing all source code for test and yaml files not relevant for now. Will only contribute tests developed by buildtest maintainers.

--- toolkit/suite/openmp/hello/src/omp_hello.c
+++ toolkit/suite/openmp/hello/src/omp_hello.c
@@ -1,15 +1,27 @@
-/******************************************************************************
- * * FILE: omp_hello.c
- * * DESCRIPTION:
- * *   OpenMP Example - Hello World - C/C++ Version
- * *   In this simple example, the master thread forks a parallel region.
- * *   All threads in the team obtain their unique thread number and print it.
- * *   The master thread only prints the total number of threads.  Two OpenMP
- * *   library routines are used to obtain the number of threads and each
- * *   thread's number.
- * * AUTHOR: Blaise Barney  5/99
- * * LAST REVISED: 04/06/05
- * ******************************************************************************/
+/*
+MIT License
+
+Copyright (c) 2017-2019, Shahzeb Siddiqui
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
