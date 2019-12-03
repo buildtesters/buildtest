d93ef618bee10b2fe1eb75c9578bd4ea82ec9ad2
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2019-12-03 09:41:28 -0500

    update github action black.yml adding install_deps.sh script to install Lmod by source used in .travis.yml Testing Travis build with two versions of Lmod.

---
+++ src/buildtest/scripts/install_deps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <version>"
+    exit 1
+fi
+VERSION=$1
+LMOD_PACKAGE=Lmod-$VERSION
+PREFIX=/opt/apps
+
+PKG_URL="https://github.com/TACC/Lmod/archive/${VERSION}.tar.gz"
+export PATH=$PREFIX/lmod/$VERSION/libexec:$PATH
+export MOD_INIT=$HOME/lmod/$VERSION/init/bash
+
+echo "Installing ${LMOD_PACKAGE} @ ${PREFIX}..."
+mkdir -p ${PREFIX}
+set +e
+wget ${PKG_URL} && tar xfz *${VERSION}.tar.gz
+set -e
+cd Lmod
+./configure --prefix=$PREFIX && make && make install
+
+if [ ! -z $MOD_INIT ]; then
+        source $MOD_INIT
+        type module
+fi
