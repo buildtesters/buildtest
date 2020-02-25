fba8a059ffff52dff3cda2b64f8501c8bf6fe6f3
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-02-25 16:14:57 -0500

    add few more exception related tests missed during coverage report

--- tests/test_modules.py
+++ tests/test_modules.py
@@ -10,7 +10,8 @@ from buildtest.tools.modules import (
 from buildtest.tools.modulesystem.module_difference import diff_trees
 from buildtest.tools.modulesystem.tree import module_tree_add, module_tree_rm
 from buildtest.tools.log import BuildTestError
-from buildtest.module import Module, ModuleCollection,get_all_collections
+from buildtest.module import Module, ModuleCollection, get_all_collections
+
 
 @pytest.mark.skip("not working")
 def test_spack_modules():
@@ -25,15 +26,20 @@ def test_module_deps():
     find_module_deps("GCCcore/8.1.0")
     module_tree_rm(["/mxg-hpc/users/ssi29/easybuild-HMNS/modules/all/Core"])
 
+
 @pytest.mark.skip("not working")
 def test_diff_trees():
-    diff_trees("/mxg-hpc/users/ssi29/easybuild-HMNS/modules/all/Core,/usr/share/lmod/lmod/modulefiles/Core/")
+    diff_trees(
+        "/mxg-hpc/users/ssi29/easybuild-HMNS/modules/all/Core,/usr/share/lmod/lmod/modulefiles/Core/"
+    )
+
 
 @pytest.mark.skip("not working")
 def test_easybuild_modules():
     module_tree_add(["/opt/easybuild/modules/all"])
     check_easybuild_module()
     module_tree_rm(["/opt/easybuild/modules/all"])
+
 
 @pytest.mark.skip("not working")
 def test_module_diff():
@@ -61,19 +67,18 @@ def test_module_diff_invalid_args():
 def test_list_all_parents():
     list_all_parent_modules()
 
-class TestModule:
 
+class TestModule:
     def test_module(self):
         mod_names = ["lmod"]
         a = Module(mod_names)
         a.get_command()
         assert 0 == a.test_modules()
 
-        b = Module(mod_names,force=True)
-        #assert 0 == b.get_command().find("module purge --force")
+        b = Module(mod_names, force=True)
         assert 0 == b.test_modules()
 
-        c = Module(mod_names,debug=True)
+        c = Module(mod_names, debug=True)
         assert 0 == c.test_modules()
 
     def test_collection(self):
@@ -87,8 +92,18 @@ class TestModule:
         # show "settarg" collection
         cmd.describe("settarg")
 
-class TestModuleCollection:
+    @pytest.mark.xfail(reason="Collection Name must be string when saving", raises=TypeError)
+    def test_type_error_save_collection(self):
+        cmd = Module(["settarg"])
+        cmd.save(1)
 
+    @pytest.mark.xfail(reason="Collection Name must be string when showing content of collection", raises=TypeError)
+    def test_type_error_describe_collection(self):
+        cmd = Module(["settarg"])
+        cmd.describe(1)
+
+
+class TestModuleCollection:
     def test_get_collection_string(self):
         a = ModuleCollection("settarg")
         assert "module restore settarg" == a.get_command()
@@ -96,9 +111,9 @@ class TestModuleCollection:
     def test_collection_exists(self):
         assert "settarg" in get_all_collections()
 
-    @pytest.mark.xfail(reason="Type error when a non-string argument to ModuleCollection class",raises=TypeError)
+    @pytest.mark.xfail(
+        reason="Type error when a non-string argument to ModuleCollection class",
+        raises=TypeError,
+    )
     def test_type_error(self):
         a = ModuleCollection(1)
-
-
-
