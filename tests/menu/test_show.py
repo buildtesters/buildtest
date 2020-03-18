daa25668a4b686ca106c532429b19c2ad858ada9
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-03-17 21:32:25 -0400

    reorg regtests based on source file to be tested  (#218)
    * reorg regtests based on source file to be tested and map directory structure similar to source code
    
    Signed-off-by: Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
    
    * move test_buildsystem.py to test/buildsystem/test_base.py
    Move test for buildtest show schema to tests/menu/test_show.py
    
    * removing test_show_schema from test_config.py. This was duplicate test
    it is now in test_show.py. The method is defined in
    buildtest/menu/show.py so the test must be in tests/menu/test_show.py

--- tests/menu/test_show.py
+++ tests/menu/test_show.py
@@ -1,10 +1,4 @@
-from buildtest.menu.config import show_configuration
 from buildtest.menu.show import show_schema_layout
-
-
-def test_show_config():
-    show_configuration()
-
 
 def test_show_schema():
     class args:
