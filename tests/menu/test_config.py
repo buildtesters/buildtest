c3eb63b2a830e3f716ece64c7fc33baf57b4ced3
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-03-17 21:23:00 -0400

    removing test_show_schema from test_config.py. This was duplicate test it is now in test_show.py. The method is defined in buildtest/menu/show.py so the test must be in tests/menu/test_show.py

--- tests/menu/test_config.py
+++ tests/menu/test_config.py
@@ -1,7 +1,6 @@
 import os
 from buildtest.defaults import BUILDTEST_CONFIG_BACKUP_FILE
 from buildtest.menu.config import show_configuration, func_config_view, func_config_restore
-from buildtest.menu.show import show_schema_layout
 
 def test_show_config():
     show_configuration()
@@ -14,7 +13,3 @@ def test_config_restore():
     # removing backup file and testing of restore works
     os.remove(BUILDTEST_CONFIG_BACKUP_FILE)
     func_config_restore()
-
-
-def test_show_schema():
-    show_schema_layout()
