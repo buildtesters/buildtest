6bc7719e51ddcc442c2a26d0ae566bc4ede17799
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-03-23 20:50:42 +0000

    Adding two example buildtest configuration to check schema. Change keyname "EDITOR" to "editor" in regards to "buildtest config edit" option.

--- tests/menu/test_config.py
+++ tests/menu/test_config.py
@@ -1,6 +1,11 @@
 import os
-from buildtest.defaults import BUILDTEST_CONFIG_BACKUP_FILE
+import pytest
+from jsonschema import validate, ValidationError
+from buildtest.defaults import BUILDTEST_CONFIG_BACKUP_FILE, DEFAULT_CONFIG_SCHEMA
 from buildtest.menu.config import show_configuration, func_config_view, func_config_restore
+from buildtest.buildsystem.schemas.utils import load_schema
+
+pytest_root = os.path.dirname(os.path.dirname(__file__))
 
 def test_show_config():
     show_configuration()
@@ -13,3 +18,23 @@ def test_config_restore():
     # removing backup file and testing of restore works
     os.remove(BUILDTEST_CONFIG_BACKUP_FILE)
     func_config_restore()
+
+
+def test_config_ex1():
+    example_schema = os.path.join(pytest_root,"config_schema_examples","example1.json")
+    schema_config = load_schema(DEFAULT_CONFIG_SCHEMA)    
+    example = load_schema(example_schema)
+    validate(instance=example,schema=schema_config)
+
+
+
+def test_config_ex2():
+    example_schema = os.path.join(pytest_root,"config_schema_examples","example2.json")
+    schema_config = load_schema(DEFAULT_CONFIG_SCHEMA)    
+    example = load_schema(example_schema)
+    try:
+        validate(instance=example,schema=schema_config)
+    except ValidationError:
+        print(f"Failed to validate configuration file: {example_schema} with schema {DEFAULT_CONFIG_SCHEMA}")
+        assert True
+
