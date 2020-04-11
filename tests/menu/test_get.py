542e243641b7a88a091dac4fbaf5869fe99ccfa9
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-04-11 16:26:57 -0400

    use tmp_path when testing cloning of tutorials repo

--- tests/menu/test_get.py
+++ tests/menu/test_get.py
@@ -1,27 +1,19 @@
-import os
 import pytest
 import shutil
-from buildtest.defaults import TESTCONFIG_ROOT
 from buildtest.menu.get import clone
 from buildtest.utils.file import is_dir, create_dir
 
 
-def test_clone():
+def test_clone(tmp_path):
     repo = "https://github.com/HPC-buildtest/tutorials.git"
-    root = os.path.join(TESTCONFIG_ROOT, "github.com")
 
-    if is_dir(root):
-        shutil.rmtree(root)
-
-    create_dir(root)
-
-    assert is_dir(clone(repo, root))
+    assert is_dir(clone(repo, tmp_path))
 
     # cloning same repo twice will result in failure
     with pytest.raises(SystemExit) as e_info:
-        clone(repo, root)
+        clone(repo, tmp_path)
 
-    shutil.rmtree(root)
+    shutil.rmtree(tmp_path)
     # will fail to clone if invalid branch is specified
     with pytest.raises(SystemExit) as e_info:
-        clone(repo, root, "develop")
+        clone(repo, tmp_path, "develop")
