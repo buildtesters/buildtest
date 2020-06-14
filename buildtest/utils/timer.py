34576a8cd81aa33b2e245f1023fd45c848f23304
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-06-13 23:54:43 -0400

    fix report by pyflakes
    Signed-off-by: Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>

--- buildtest/utils/timer.py
+++ buildtest/utils/timer.py
@@ -12,14 +12,14 @@ class Timer:
     def start(self):
         """Start a new timer"""
         if self._start_time is not None:
-            raise TimerError(f"Timer is running. Use .stop() to stop it")
+            raise TimerError("Timer is running. Use .stop() to stop it")
 
         self._start_time = time.perf_counter()
 
     def stop(self):
         """Stop the timer, and report the elapsed time"""
         if self._start_time is None:
-            raise TimerError(f"Timer is not running. Use .start() to start it")
+            raise TimerError("Timer is not running. Use .start() to start it")
 
         elapsed_time = time.perf_counter() - self._start_time
         self._start_time = None
