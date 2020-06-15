d863e6e87aa65b9487f8893b651566bcce1aec48
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-06-14 21:00:41 -0400

    removing run_tests script this is not used for any useful purpose

--- tests/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# This run_tests.sh file is intended to invoke python testing of buildtest.
-# It is required that buildtest is installed, or interacted with 
-# from the root of the development directory.
-
-
-# Run tests just for everything else
-pytest -vra tests/
