5ecca438d64109fd366c73efa3bf9c5295153436
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-07-17 01:00:56 -0400

    add coverage for test_report.py

--- tests/menu/test_report.py
+++ tests/menu/test_report.py
@@ -3,6 +3,26 @@ import pytest
 from buildtest.defaults import BUILD_REPORT
 from buildtest.menu.report import func_report
 
+def test_report_format():
+
+    class args:
+        helpformat = False
+        format = None
+
+    func_report(args)
+
+    class args:
+        helpformat = False
+        format = "name,state,returncode,buildspec"
+
+    func_report(args)
+
+    class args:
+        helpformat = False
+        format = "badfield,state,returncode"
+
+    with pytest.raises(SystemExit):
+        func_report(args)
 
 def test_func_report_when_BUILD_REPORT_missing():
 
@@ -13,3 +33,10 @@ def test_func_report_when_BUILD_REPORT_missing():
 
     with pytest.raises(SystemExit):
         func_report()
+
+def test_report_helpformat():
+
+    class args:
+        helpformat = True
+        format = None
+    func_report(args)
