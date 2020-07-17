a0f852daf9ee58d0d72899168b31397b5ecbeb8b
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-07-17 01:11:10 -0400

    create backup for BUILD_REPORT and run copy and move in try/except

--- tests/menu/test_report.py
+++ tests/menu/test_report.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+from shutil import copy
 from buildtest.defaults import BUILD_REPORT
 from buildtest.menu.report import func_report
 
@@ -24,21 +25,30 @@ def test_report_format():
     with pytest.raises(SystemExit):
         func_report(args)
 
-
-def test_func_report_when_BUILD_REPORT_missing():
-
-    try:
-        os.remove(BUILD_REPORT)
-    except OSError:
-        pass
-
-    with pytest.raises(SystemExit):
-        func_report()
-
-
 def test_report_helpformat():
     class args:
         helpformat = True
         format = None
 
     func_report(args)
+
+def test_func_report_when_BUILD_REPORT_missing():
+
+    backupfile = f"{BUILD_REPORT}.bak"
+
+    try:
+        copy(BUILD_REPORT, backupfile)
+        os.remove(BUILD_REPORT)
+    except OSError:
+        pass
+
+    with pytest.raises(SystemExit):
+        func_report()
+    try:
+        move(backupfile, BUILD_REPORT)
+    except:
+        pass
+
+    assert BUILD_REPORT
+
+
