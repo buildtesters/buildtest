8db8849d401a085ad66de079f5235e0edec9335d
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-09-09 16:02:12 -0400

    Add method build_phase and resolve_testdirectory by moving implementation from main class func_build_subcmd into methods so that each method can be called indepedently so that one can use it for scripting buildspecs add test script workflow that will run all tests in scripts directory. Moved all test from scripts/discover_buildspecs to scripts directory.

---
+++ scripts/ex3.py
@@ -0,0 +1,15 @@
+from buildtest.config import load_settings
+from buildtest.menu.build import discover_buildspecs, resolve_testdirectory, build_phase
+
+from buildtest.menu.buildspec import parse_buildspecs
+
+tagname = "tutorials"
+print(f"Searching by tagname: {tagname}")
+included_bp, excluded_bp = discover_buildspecs(tags=tagname)
+print(f"discovered_buildspec: {included_bp} \n\n")
+
+configuration = load_settings()
+testdir = resolve_testdirectory(configuration)
+builders = parse_buildspecs(included_bp, testdir, printTable=True)
+
+build_phase(builders, printTable=True)
