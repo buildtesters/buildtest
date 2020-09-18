9b45e396b5898fd0e7ccce57a9b9b1041478945e
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-09-18 12:55:15 -0400

    change output table columns for buildtest build Add some clarification in getting started guide when building by tags and executors with respect to behavior of --tags and --executor when it comes to discovering and filtering test rebuild documentation examples

--- buildtest/docgen/main.py
+++ buildtest/docgen/main.py
@@ -119,11 +119,11 @@ def introspection_cmds():
         "buildspec_multifield_filter.txt": "buildtest buildspec find --filter tags=tutorials,executor=local.sh,type=script",
         "report-helpformat.txt": "buildtest report --helpformat",
         "report-helpfilter.txt": "buildtest report --helpfilter",
-        "report-format.txt": "buildtest report --format name,schemafile,executor,state,returncode",
-        "report-filter-name.txt": "buildtest report --filter name=exit1_pass --format=name,returncode,state",
+        "report-format.txt": "buildtest report --format id,executor,state,returncode",
+        "report-filter-name.txt": "buildtest report --filter name=exit1_pass --format=id,returncode,state",
         "report-filter-buildspec.txt": "buildtest report --filter buildspec=tutorials/pass_returncode.yml --format=name,state,buildspec",
-        "report-multifilter.txt": "buildtest report --filter state=FAIL,executor=local.sh --format=name,state,executor",
-        "report-returncode.txt": "buildtest report --filter returncode=2 --format=name,returncode",
+        "report-multifilter.txt": "buildtest report --filter state=FAIL,executor=local.sh --format=id,state,executor",
+        "report-returncode.txt": "buildtest report --filter returncode=2 --format=id,returncode",
     }
 
     for k, v in cmd_dict.items():
