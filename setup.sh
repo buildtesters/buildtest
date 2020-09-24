a351f12227f9e02d3d48603092425db6d152402c
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-09-24 16:41:03 -0400

    add setup.sh used for installing buildtest.  (#491)
    * add setup.sh used for installing buildtest. Only tested with bash.
    bin/buildtest invokes buildtest.
    Add requirements.txt in top-level folder that is used for installing
    buildtest dependency in setup.sh
    
    * update installation for buildtest in workflows
    
    * add PYTHONPATH to buildtest source code

---
+++ setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# get platform
+platform=$(python -c "import platform; print(platform.system())")
+
+#echo "${BASH_SOURCE[0]}"
+#echo "platform:" $platform
+
+#if [ "$platform" == "Darwin" ]; then
+#  realpath=$(grealpath ${BASH_SOURCE[0]})
+#  echo realpath
+#  buildtest_root="$(dirname "$(grealpath ${BASH_SOURCE[0]})")"
+#fi
+
+buildtest_root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
+echo "buildtest root is ${buildtest_root}"
+
+echo "Installing buildtest dependencies"
+pip install -r ${buildtest_root}/requirements.txt &> /dev/null
+
+bin=${buildtest_root}/bin
+export PATH=${bin}:$PATH
+# add PYTHONPATH for buildtest to persist in shell environment
+export PYTHONPATH=${buildtest_root}/buildtest:$PYTHONPATH
+
+buildtest_path=$(which buildtest)
+echo "Adding ${bin} to PATH variable"
+echo "buildtest command: ${buildtest_path}"
