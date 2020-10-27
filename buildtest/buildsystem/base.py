7c1cd2725d2b925644ef3e67b895702ec823de9d
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-10-27 19:36:01 -0400

    rename variable can't use vars as a variable name

--- buildtest/buildsystem/base.py
+++ buildtest/buildsystem/base.py
@@ -303,7 +303,7 @@ class BuilderBase:
            :rtype: list
         """
 
-        vars = []
+        variables = []
         pairs = self.recipe.get("vars", [])
         shell = self.shell.name
         # Parse environment depending on expected shell
@@ -312,19 +312,19 @@ class BuilderBase:
             # bash, sh, zsh variable declaration is KEY=VALUE
             if re.fullmatch("(bash|sh|zsh|/bin/bash|/bin/sh|/bin/zsh)$", shell):
                 for k, v in pairs.items():
-                    vars.append("%s=$s" % (k, v))
+                    variables.append("%s=$s" % (k, v))
 
 
             # tcsh, csh variable declaration is set KEY=VALUE
             elif re.fullmatch("(tcsh|csh|/bin/tcsh|/bin/csh)$", shell):
                 for k, v in pairs.items():
-                    vars.append("set %s=$s" % (k, v))
+                    variables.append("set %s=$s" % (k, v))
 
             else:
                 self.logger.warning(
                     f"{shell} is not supported, skipping environment variables."
                 )
-        return vars
+        return variables
 
     def _generate_unique_id(self):
         """Generate a build id based on the Buildspec name, and datetime."""
