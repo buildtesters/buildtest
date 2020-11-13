05f67d19556ab934a19ecdf90cea913c214d5ce1
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2020-11-13 18:02:27 -0500

    refactor BuildspecParser class by adding methods to class. The get_builders method was doing too many things. We have separated filter logic into separate class methods that are private. The _validate_global method is removed and global validation is part of _validate class. Add invalid and valid buildspecs for testing BuildspecParser class for exceptions.

--- buildtest/schemas/defaults.py
+++ buildtest/schemas/defaults.py
@@ -13,13 +13,16 @@ schema_table["names"] = [
     "compiler-v1.0.schema.json",
     "script-v1.0.schema.json",
 ]
+schema_table["versions"] = {}
+schema_table["versions"]["script"] = ["1.0"]
+schema_table["versions"]["compiler"] = ["1.0"]
+
 schema_table["global.schema.json"] = {}
 schema_table["global.schema.json"]["path"] = os.path.join(here, "global.schema.json")
 schema_table["global.schema.json"]["recipe"] = load_schema(
     schema_table["global.schema.json"]["path"]
 )
 schema_table["script-v1.0.schema.json"] = {}
-schema_table["script-v1.0.schema.json"]["versions"] = ["1.0"]
 schema_table["script-v1.0.schema.json"]["path"] = os.path.join(
     here, "script-v1.0.schema.json"
 )
@@ -27,7 +30,6 @@ schema_table["script-v1.0.schema.json"]["recipe"] = load_schema(
     schema_table["script-v1.0.schema.json"]["path"]
 )
 schema_table["compiler-v1.0.schema.json"] = {}
-schema_table["compiler-v1.0.schema.json"]["versions"] = ["1.0"]
 schema_table["compiler-v1.0.schema.json"]["path"] = os.path.join(
     here, "compiler-v1.0.schema.json"
 )
