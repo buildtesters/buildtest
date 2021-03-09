7cd04c1d07ef95da81bc3f32db05eb24ace668c7
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2021-03-09 15:41:00 -0500

    add jlse gitlab CI file add example settings for jlse system for regression test add regression test for running a single hostname job on JLSE using cobalt scheduler Rename directory cori_buildspecs to cori Rewrite conditional skip for ascent, and cori to use socket.getfqdn() to match hostname and domain search when skipping test.

--- tests/test_cori.py
+++ tests/test_cori.py
@@ -1,16 +1,21 @@
 import os
 import pytest
+import socket
 from buildtest.menu.build import BuildTest
 
 
 def test_cori():
-
-    if os.getenv("NERSC_HOST") != "cori":
-        pytest.skip("Test runs only on Cori")
+    # This test must run on Cori Login nodes which are cori[01-20].nersc.gov.
+    if not socket.getfqdn().endswith("nersc.gov") or not socket.getfqdn().startswith(
+        "cori"
+    ):
+        pytest.skip(
+            "This test runs only on domain 'nersc.gov' with machine names that start with 'cori*'"
+        )
 
     here = os.path.dirname(os.path.abspath(__file__))
-    cori_configuration = os.path.join(here, "settings", "cori.config.yml")
+    cori_configuration = os.path.join(here, "settings", "cori.yml")
 
-    buildspec_files = os.path.join(here, "examples", "cori_buildspecs", "hostname.yml")
+    buildspec_files = os.path.join(here, "examples", "cori", "hostname.yml")
     cmd = BuildTest(config_file=cori_configuration, buildspecs=[buildspec_files])
     cmd.build()
---
+++ tests/test_jlse.py
@@ -0,0 +1,17 @@
+import os
+import pytest
+import socket
+from buildtest.menu.build import BuildTest
+
+
+def test_jlse():
+
+    if not socket.getfqdn().endswith("alcf.anl.gov"):
+        pytest.skip("Test runs only on JLSE Login Nodes with domain name alcf.anl.gov")
+
+    here = os.path.dirname(os.path.abspath(__file__))
+    configuration = os.path.join(here, "settings", "jlse.yml")
+
+    buildspec_files = os.path.join(here, "examples", "jlse", "hostname.yml")
+    cmd = BuildTest(config_file=configuration, buildspecs=[buildspec_files])
+    cmd.build()
