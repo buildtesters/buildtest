ac27b5c0da5ec6ca4497407818d8331174d9a750
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2021-04-13 13:52:21 -0400

    remove unused import

--- buildtest/cli/cdash.py
+++ buildtest/cli/cdash.py
@@ -10,7 +10,7 @@ import zlib
 
 from datetime import datetime
 from urllib.request import urlopen, Request
-from urllib.parse import urlencode, urljoin
+from urllib.parse import urlencode
 from buildtest.defaults import BUILD_REPORT
 from buildtest.utils.tools import deep_get
 
