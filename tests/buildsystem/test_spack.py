5140844030ff6075ed450d710746283f826fe136
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2021-07-29 15:44:58 -0400

    add regression test to run spack examples to increase test coverage for SpackBuilder class

---
+++ tests/buildsystem/test_spack.py
@@ -0,0 +1,24 @@
+from buildtest.cli.build import BuildTest
+from buildtest.cli.buildspec import BuildspecCache
+from buildtest.config import SiteConfiguration
+from buildtest.system import BuildTestSystem
+
+configuration = SiteConfiguration()
+configuration.detect_system()
+configuration.validate()
+
+
+def test_spack_examples():
+    system = BuildTestSystem()
+    system.check()
+
+    # ensure we rebuild cache file before running any buildspecs commands
+    BuildspecCache(rebuild=True, configuration=configuration)
+
+    cmd = BuildTest(
+        configuration=configuration,
+        tags=["spack"],
+        stage="build",
+        buildtest_system=system,
+    )
+    cmd.build()
