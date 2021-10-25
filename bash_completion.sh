ef2cad21ffdbc908d59f4413774345b3a0d8eb0e
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2021-10-25 13:27:13 -0400

    add 'buildtest config edit' command to interactively open configuration file in editor update bash completion script with new command

--- bash_completion.sh
+++ bash_completion.sh
@@ -130,7 +130,7 @@ _buildtest ()
       COMPREPLY=( $( compgen -W "$opts" -- $cur ) );;
 
     config|cg)
-      local cmds="-h --help compilers executors validate view summary systems"
+      local cmds="-h --help compilers edit executors validate view summary systems"
 
       COMPREPLY=( $( compgen -W "${cmds}" -- $cur ) )
       # handle completion logic for 'buildtest config <subcommand>' based on subcommands
