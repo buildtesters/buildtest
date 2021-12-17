576c8618b9f0ece0dbcfbcdeacd39255decbcaa4
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2021-12-16 19:56:25 -0500

    add regression test coverage for buildtest history query --output. Also we add coverage for Tee class which was required to capture output from buildtest build

--- tests/cli/test_history.py
+++ tests/cli/test_history.py
@@ -1,8 +1,15 @@
+import os
+import shutil
+
 import pytest
-from buildtest.cli.history import build_history, query_builds
+from buildtest.cli.build import BuildTest, Tee
+from buildtest.cli.history import BUILD_HISTORY_DIR, build_history, query_builds
+from buildtest.config import SiteConfiguration
+from buildtest.defaults import BUILDTEST_ROOT, VAR_DIR
+from buildtest.system import BuildTestSystem
 
 
-def test_build_history():
+def test_build_history_list():
     class args:
         history = "list"
         terse = False
@@ -27,16 +34,55 @@ def test_build_history():
     # 'buildtest build history list --terse --no-header'
     build_history(args)
 
+
+def test_build_history_query():
+
+    fname = os.path.join(VAR_DIR, "output.txt")
+    configuration = SiteConfiguration()
+
+    configuration.detect_system()
+    configuration.validate()
+
+    system = BuildTestSystem()
+
+    with Tee(fname):
+        cmd = BuildTest(
+            buildspecs=[os.path.join(BUILDTEST_ROOT, "tutorials", "vars.yml")],
+            buildtest_system=system,
+            configuration=configuration,
+        )
+        cmd.build()
+
+    build_history_dir = cmd.get_build_history_dir()
+    shutil.move(fname, os.path.join(build_history_dir, "output.txt"))
+
+    num_ids = list(range(len(os.listdir(BUILD_HISTORY_DIR))))[-1]
+    print(num_ids)
+
     class args:
         history = "query"
-        id = 0
+        id = num_ids
         log = False
+        output = False
 
-    # 'buildtest build history query 0'
+    # 'buildtest build history query <ID>'
+    build_history(args)
+
+    class args:
+        history = "query"
+        id = num_ids
+        log = False
+        output = True
+
+    # 'buildtest build history query --output <ID>'
     build_history(args)
 
 
 def test_invalid_buildid():
 
     with pytest.raises(SystemExit):
-        query_builds(build_id=-1, log_option=True)
+        query_builds(build_id=-1, log_option=True, output=False)
+
+    shutil.rmtree(BUILD_HISTORY_DIR)
+    with pytest.raises(SystemExit):
+        query_builds(build_id=0, log_option=False, output=False)
