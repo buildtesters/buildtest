e7144d060ac41c5b0b654cdd82128f93933bf579
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2022-03-01 16:59:43 -0500

    fix issue with how local executor was calling the shell, we had all test running with 'bash' when using different shell types. With this change we have fixed the invocation call and refactored the codebase. Minor update to user documentation with typo 'max_pend_time' should be 'maxpendtime'

--- buildtest/executors/lsf.py
+++ buildtest/executors/lsf.py
@@ -106,8 +106,7 @@ class LSFExecutor(BaseExecutor):
         os.chdir(builder.stage_dir)
         self.logger.debug(f"Changing to stage directory {builder.stage_dir}")
 
-        cmd = self.bash_launch_command() + [os.path.basename(builder.build_script)]
-        cmd = " ".join(cmd)
+        cmd = f"bash {self._bashopts} {os.path.basename(builder.build_script)}"
 
         try:
             command = builder.run(cmd)
