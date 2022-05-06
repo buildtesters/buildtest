88fd3c0f1fe9e7fab04a6b77e902bf4cc216447c
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2022-05-06 17:16:53 -0400

    update dockerfile and add spack site configuration scope. (#1053)

--- scripts/spack_container/spack_setup.sh
+++ scripts/spack_container/spack_setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-. ~/spack/share/spack/setup-env.sh
+. /home/spack/spack/share/spack/setup-env.sh
 . $(spack location -i lmod)/lmod/lmod/init/bash
 spack module tcl refresh --delete-tree -y
