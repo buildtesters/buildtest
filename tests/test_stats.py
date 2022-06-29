a604a2149d4c5fbf6a6f8ef975c30b7942c42601
Sean Xiang <sijiex@andrew.cmu.edu>
2022-06-29 12:30:08 -0400

    add test_stats.py file

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -1,6 +1,22 @@
+from buildtest.cli.build import BuildTest
+from buildtest.cli.buildspec import BuildspecCache
+from buildtest.cli.report import Report
 from buildtest.cli.stats import stats_cmd
+from buildtest.config import SiteConfiguration
+from buildtest.system import BuildTestSystem
 
 
 def test_stats():
-    name = "exit1_fail"
-    stats_cmd(name)
+    configuration = SiteConfiguration()
+    configuration.detect_system()
+    configuration.validate()
+    system = BuildTestSystem()
+
+    BuildspecCache(rebuild=True, configuration=configuration)
+
+    cmd = BuildTest(configuration=configuration, tags=["pass"], buildtest_system=system)
+    cmd.build()
+
+    report = Report()
+    name = report.get_random_tests()
+    stats_cmd(name[0])
