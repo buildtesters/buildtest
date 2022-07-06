7d0e12053e4fc71959a605c2243281e500ed0f64
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2022-07-06 16:24:34 -0400

    remove commented lines

--- buildtest/cli/info.py
+++ buildtest/cli/info.py
@@ -48,26 +48,18 @@ def buildtest_info(configuration, buildtest_system):
         buildtest_details += f"[red]Build History Directory:[/red]  [green]{BUILD_HISTORY_DIR}[/green] \n"
         buildtest_details += f"[red]Number of builds:[/red]         [green]{len(os.listdir(BUILD_HISTORY_DIR))}[/green] \n"
 
-        # console.print(f"Build History Directory: {BUILD_HISTORY_DIR}")
-
-        # console.print(f"Number of builds in history: {len(os.listdir(BUILD_HISTORY_DIR))}")
-
     if is_file(BUILDSPEC_CACHE_FILE):
         buildtest_details += f"[red]Buildspec Cache File:[/red]     [green]{BUILDSPEC_CACHE_FILE}[/green] \n"
-        # console.print(f"Buildspec Cache File: {BUILDSPEC_CACHE_FILE}")
     else:
         buildtest_details += "[red]Buildspec Cache File does not exist"
-
-        # console.print(f"Buildspec Cache File does not exist")
 
     if is_file(BUILD_REPORT):
         buildtest_details += (
             f"[red]Default Report File:[/red]      [green]{BUILD_REPORT}[/green]"
         )
-        # console.print(f"Default Report File: {BUILD_REPORT}")
+
     else:
         buildtest_details += "[red]Default report file does not exist"
-        # console.print(f"Default report file does not exist")
 
     console.print(
         Panel.fit(buildtest_details, title="buildtest details"), justify="left"
