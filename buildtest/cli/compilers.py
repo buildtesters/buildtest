62712c9e7565a826b8678295e4aa214cdf5bfd34
Sean Xiang <sijiex@andrew.cmu.edu>
2022-08-24 16:01:25 -0400

    ådd module path

--- buildtest/cli/compilers.py
+++ buildtest/cli/compilers.py
@@ -217,7 +217,7 @@ class BuildtestCompilers:
         if self.moduletool == "lmod":
             if self.detailed:
                 print("Searching modules via Lmod Spider")
-            spider = Spider(tree="/usr/share/lmod/lmod/modulefiles/Core")
+            spider = Spider(tree="/opt/cray/pe/lmod/modulefiles/mpi/gnu/8.0/ofi/1.0/cray-mpich/8.0")
 
             print("spider.get_modules(): ", spider.get_modules())
 
