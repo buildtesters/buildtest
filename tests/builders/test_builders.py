93ef832ee579e0fa6142b907bb0a50f0b2067088
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2023-01-04 16:32:59 -0500

    add regression test for checking exists and is_file and is_dir

--- tests/builders/test_builders.py
+++ tests/builders/test_builders.py
@@ -39,3 +39,21 @@ def test_assert_range():
         configuration=config,
     )
     cmd.build()
+
+
+def test_assert_exists():
+    cmd = BuildTest(
+        buildspecs=[os.path.join(here, "exists.yml")],
+        buildtest_system=system,
+        configuration=config,
+    )
+    cmd.build()
+
+
+def test_assert_is_file_is_dir():
+    cmd = BuildTest(
+        buildspecs=[os.path.join(here, "file_and_dir_check.yml")],
+        buildtest_system=system,
+        configuration=config,
+    )
+    cmd.build()
