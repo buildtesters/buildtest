a7b1e678d11f1630316cb7b569622aa2491a47bb
Mariam Ajibade <ajibade.mariam@philander.edu>
2023-07-11 15:23:52 -0500

    removed option --disable-executor-check from buildtest/cli/__init__.py

--- buildtest/cli/__init__.py
+++ buildtest/cli/__init__.py
@@ -670,7 +670,6 @@ def build_menu(subparsers):
         type=positive_number,
     )
     extra_group.add_argument(
-        "--disable-executor-check",
         action="store_false",
         help="Disable executor check during configuration check. By default these checks are enforced for Local, Slurm, PBS, LSF, and Cobalt Executor.",
     )
