6378e500636f41b499d69cc229a40522232e0d47
Mariam Ajibade <ajibade.mariam@philander.edu>
2023-07-12 15:01:10 -0500

    removed validate method from tests/executors/test_base.py

--- tests/executors/test_base.py
+++ tests/executors/test_base.py
@@ -19,7 +19,6 @@ pytest_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 def test_build_executor(tmp_path):
     bc = SiteConfiguration()
     bc.detect_system()
-    bc.validate(validate_executors=True)
 
     # Load BuildExecutor
     be = BuildExecutor(bc)
