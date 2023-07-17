e755449273def38f1626b1b1c4350fe9bc45cecf
braeeast2001 <beast@lbl.gov>
2023-07-17 11:55:16 -0500

    Update commands.py
    Removed table; Pritning raw list

--- buildtest/cli/commands.py
+++ buildtest/cli/commands.py
@@ -1,33 +1,12 @@
-from buildtest.defaults import console
-from rich.table import Table
-
-
 def commands_cmd():
     """This method implements command ``buildtest commands`` which shows a list
     of buildtest commands"""
 
-    table = Table(title=None, show_lines=False)
-    table.add_column(None, justify="left", style="magenta")
+    cmds_list = ["build", "buildspec", "config", "report", "inspect",
+                 "path", "history", "schemacdash", "unittests",
+                 "stylecheck", "cd", "clean", "docs", "schemadocs",
+                 "debugreport", "stats" "info", "help",
+                 "tutorial-examples"]
 
-    table.add_row("build")
-    table.add_row("buildspec")
-    table.add_row("config")
-    table.add_row("report")
-    table.add_row("inspect")
-    table.add_row("path")
-    table.add_row("history")
-    table.add_row("schema")
-    table.add_row("cdash")
-    table.add_row("unittests")
-    table.add_row("stylecheck")
-    table.add_row("cd")
-    table.add_row("clean")
-    table.add_row("docs")
-    table.add_row("schemadocs")
-    table.add_row("debugreport")
-    table.add_row("stats")
-    table.add_row("info")
-    table.add_row("help")
-    table.add_row("tutorial-examples")
-
-    console.print(table)
+    for x in cmds_list:
+        print(x, "\n")
