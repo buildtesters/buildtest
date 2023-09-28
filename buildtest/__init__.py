769177108969e950658d5244d16ceb990c57a9d7
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2023-09-28 15:30:15 -0400

    bump version to 1.6

--- buildtest/__init__.py
+++ buildtest/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "1.5"
+__version__ = "1.6"
 BUILDTEST_VERSION = __version__
 BUILDTEST_COPYRIGHT = "Copyright (c) 2021-2023, The Regents of the University of California, through Lawrence Berkeley National Laboratory (subject to receipt of any required approvals from the U.S. Dept. of Energy), Shahzeb Siddiqui, and Vanessa Sochat. All rights reserved."
