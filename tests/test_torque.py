065f1c388700f9dd374d7b9c1d769df75f40b05a
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2024-04-02 13:12:03 +0000

    add regression test examples for running torque examples add example buildspecs that will be run by job submission to scheduler add invalid executor configuration file to test functionality when executor is not found.

---
+++ tests/test_torque.py
@@ -0,0 +1,63 @@
+import os
+
+import pytest
+
+from buildtest.cli.build import BuildTest
+from buildtest.config import SiteConfiguration
+from buildtest.exceptions import BuildTestError
+from buildtest.scheduler.detection import Torque
+from buildtest.system import BuildTestSystem
+
+
+def test_torque():
+    torque = Torque()
+    if not torque.active():
+        pytest.skip("Test must run on torque scheduler")
+
+    here = os.path.dirname(os.path.abspath(__file__))
+    settings_file = os.path.join(here, "settings", "torque.yml")
+    system = BuildTestSystem()
+
+    bc = SiteConfiguration(settings_file)
+    bc.detect_system()
+    bc.validate(moduletool="environment-modules")
+
+    cmd = BuildTest(
+        configuration=bc,
+        buildspecs=[os.path.join(here, "examples", "torque", "sleep.yml")],
+        poll_interval=5,
+    )
+    cmd.build()
+
+    with pytest.raises(BuildTestError):
+        cmd = BuildTest(
+            configuration=bc,
+            buildspecs=[os.path.join(here, "examples", "torque", "sleep_cancel.yml")],
+            poll_interval=1,
+            maxpendtime=2,
+        )
+        cmd.build()
+
+    cmd = BuildTest(
+        configuration=bc,
+        buildspecs=[os.path.join(here, "examples", "torque", "sleep.yml")],
+        buildtest_system=system,
+        numprocs=[1, 2, 4],
+        poll_interval=5,
+    )
+    cmd.build()
+
+    def test_invalid_executor():
+
+        torque = Torque()
+        if not torque.active():
+            pytest.skip("Test must run on torque scheduler")
+
+        here = os.path.dirname(os.path.abspath(__file__))
+        settings_file = os.path.join(
+            here, "settings", "invalid", "torque_invalid_executor.yml"
+        )
+
+        bc = SiteConfiguration(settings_file)
+        bc.detect_system()
+        bc.validate(moduletool="environment-modules")
