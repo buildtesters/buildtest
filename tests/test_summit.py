0f9d7231301b35381dfe7552f014fc33838765c2
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2024-04-08 13:03:58 -0400

    port example test from ascent to summit system by renaming files and updating variable references Also we had to update the project name now that we have a new project code to submit jobs to summit

--- tests/test_summit.py
+++ tests/test_summit.py
@@ -11,40 +11,37 @@ from buildtest.config import SiteConfiguration
 hostname = socket.getfqdn()
 here = os.path.dirname(os.path.abspath(__file__))
 
+settings_file = os.path.join(here, "settings", "summit.yml")
 
-def test_ascent():
-    # this test must run on Ascent system with domain '.ascent.olcf.ornl.gov' otherwise its skipped
+def test_summit():
+    # this test must run on Ascent system with domain '.summit.olcf.ornl.gov' otherwise its skipped
 
-    if not hostname.endswith("ascent.olcf.ornl.gov"):
-        pytest.skip("This test must run on domain ascent.olcf.ornl.gov")
-
-    settings_file = os.path.join(here, "settings", "ascent.yml")
+    if not hostname.endswith("summit.olcf.ornl.gov"):
+        pytest.skip("This test must run on domain summit.olcf.ornl.gov")
 
     bc = SiteConfiguration(settings_file)
     bc.detect_system()
     bc.validate(moduletool="lmod")
     BuildspecCache(rebuild=True, configuration=bc)
 
-    ascent_examples_dir = os.path.join(here, "examples", "ascent")
+    examples_dir = os.path.join(here, "examples", "summit")
     buildspec_files = [
-        os.path.join(ascent_examples_dir, "hostname.yml"),
-        os.path.join(ascent_examples_dir, "lsf_job_state.yml"),
+        os.path.join(examples_dir, "hostname.yml"),
+        os.path.join(examples_dir, "lsf_job_state.yml"),
     ]
     cmd = BuildTest(configuration=bc, buildspecs=buildspec_files)
     cmd.build()
 
     # This job will be held indefinitely but job will be cancelled by scheduler after 15sec once job pending time has reached maxpendtime
-    buildspec_files = [os.path.join(ascent_examples_dir, "hold_job.yml")]
+    buildspec_files = [os.path.join(examples_dir, "hold_job.yml")]
     cmd = BuildTest(configuration=bc, buildspecs=buildspec_files, maxpendtime=15)
     with pytest.raises(SystemExit):
         cmd.build()
 
 
 def test_compilers_find_ascent():
-    if not hostname.endswith("ascent.olcf.ornl.gov"):
-        pytest.skip("This test must run on domain ascent.olcf.ornl.gov")
-
-    settings_file = os.path.join(here, "settings", "ascent.yml")
+    if not hostname.endswith("summit.olcf.ornl.gov"):
+        pytest.skip("This test must run on domain summit.olcf.ornl.gov")
 
     config = SiteConfiguration(settings_file)
     config.detect_system()
