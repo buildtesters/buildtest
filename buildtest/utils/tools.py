64ae60b1a60165ca5a9256fdc2334d675245bf62
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2024-04-18 18:52:29 +0000

    add method check_binaries that will be used for returning a list of binary paths as a dictionary. An optional argument custom_dirs can be used to search for binary in additional paths that will be appended to PATH

--- buildtest/utils/tools.py
+++ buildtest/utils/tools.py
@@ -1,6 +1,13 @@
+import logging
+import os
+import shutil
 from functools import reduce
 
 from rich.color import Color, ColorParseError
+
+from buildtest.utils.file import is_dir, resolve_path
+
+logger = logging.getLogger(__name__)
 
 
 def deep_get(dictionary, *keys):
@@ -28,3 +35,44 @@ def checkColor(colorArg):
         except ColorParseError:
             checkedColor = Color.default().name
         return checkedColor
+
+
+def check_binaries(binaries, custom_dirs=None):
+    """Check if binaries exist in $PATH and any additional directories specified by custom_dirs. The return is a dictionary
+    containing the binary name and full path to binary.
+
+    Args:
+        binaries (list): list of binaries to check for existence in $PATH
+        custom_dirs (list, optional): list of custom directories to check for binaries. Defaults to None.
+
+    Returns:
+        dict: dictionary containing binary name and full path to binary
+    """
+
+    logger.debug(f"Check the following binaries {binaries} for existence.")
+
+    paths = os.getenv("PATH").split(os.pathsep)
+
+    if custom_dirs:
+        resolved_path = resolve_path(custom_dirs)
+        if is_dir(resolved_path):
+            paths.append(resolved_path)
+
+            logger.debug(
+                f"Adding directories {resolved_path} to PATH to check for binaries"
+            )
+
+    # convert list back to str with colon separated list of directory paths
+    paths = ":".join(paths)
+    logger.debug(f"Checking PATH directories: {paths}")
+
+    sched_dict = {}
+    for command in binaries:
+        command_fpath = shutil.which(command, path=paths)
+        if not command_fpath:
+            logger.error(f"Cannot find {command} command")
+
+        sched_dict[command] = command_fpath
+        logger.debug(f"{command}: {command_fpath}")
+
+    return sched_dict
