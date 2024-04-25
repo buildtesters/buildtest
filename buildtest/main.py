67a60f7bc6108daa071d8df3e5a912161d9c56b6
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2024-04-25 13:18:33 -0400

    refactor main method of buildtest by creating methods for handling logic for specific commands. Add setup() method to handle logic for start of buildtest environment

--- buildtest/main.py
+++ buildtest/main.py
@@ -72,36 +72,10 @@ def main():
 
     parser = BuildTestParser()
     args = parser.parse()
-    install(show_locals=True)
-    no_color = False
+    system, configuration, buildtest_editor, report_file = setup(args)
 
-    # disable color if buildtest --no-color or BUILDTEST_COLOR=False is set
-    if args.no_color or os.getenv("BUILDTEST_COLOR") == "False":
-        no_color = True
-
-    console.no_color = no_color
-
-    if args.verbose:
-        console.print("Coloring mode: ", "off" if no_color else "on")
-
-    report_file = args.report
-
-    # print content of BUILDTEST_LOGFILE if buildtest --view-log or buildtest --print-log is specified which should contain output of last log
-    if args.view_log or args.print_log:
-        # if logfile is not present we should raise exception since this file is only created upon running 'buildtest build'
-        if not is_file(BUILDTEST_LOGFILE):
-            raise BuildTestError(
-                f"Unable to find logfile: {BUILDTEST_LOGFILE}, please run 'buildtest build'"
-            )
-
-        content = read_file(BUILDTEST_LOGFILE)
-
-        if args.view_log:
-            with console.pager():
-                console.print(content)
-        else:
-            console.print(content)
-
+    # print logfile if one specifies buildtest --view-log or buildtest --print-log
+    if handle_log_viewing(args):
         return
 
     if args.listopts:
@@ -116,6 +90,133 @@ def main():
     if args.logpath:
         console.print(BUILDTEST_LOGFILE)
         return
+
+    # buildtest build command
+    if args.subcommands in ["build", "bd"]:
+        handle_build_command(args, configuration, report_file)
+
+    # buildtest build history
+    if args.subcommands in ["history", "hy"]:
+        build_history(args)
+
+    # implementation for 'buildtest buildspec find'
+    elif args.subcommands in ["buildspec", "bc"]:
+        handle_buildspec_command(args, configuration, report_file, buildtest_editor)
+
+    # running buildtest inspect
+    elif args.subcommands in ["inspect", "it"]:
+        handle_inspect_command(args, configuration, report_file)
+
+    elif args.subcommands in ["stats"]:
+        stats_cmd(name=args.name, configuration=configuration, report_file=report_file)
+
+    # running buildtest config
+    elif args.subcommands in ["config", "cg"]:
+        #  running buildtest config compilers
+        if args.config in ["compilers", "co"]:
+            compiler_cmd(args, configuration)
+        else:
+            config_cmd(
+                command_args=args,
+                configuration=configuration,
+                editor=buildtest_editor,
+                system=system,
+            )
+
+    # buildtest report
+    elif args.subcommands in ["report", "rt"]:
+        report_cmd(args, configuration=configuration, report_file=report_file)
+
+    elif args.subcommands == "path":
+        path_cmd(
+            name=args.name,
+            outfile=args.outfile,
+            errfile=args.errfile,
+            testpath=args.testpath,
+            buildscript=args.buildscript,
+            stagedir=args.stagedir,
+            buildenv=args.buildenv,
+            configuration=configuration,
+            report_file=report_file,
+        )
+    # running buildtest schema
+    elif args.subcommands == "schema":
+        schema_cmd(args)
+
+    # running buildtest cdash
+    elif args.subcommands == "cdash":
+        handle_cdash_command(args, configuration, report_file)
+
+    elif args.subcommands in ["show", "s"]:
+        buildtest_show(command=args.command)
+
+    elif args.subcommands == "clean":
+        clean(configuration=configuration, yes=args.yes)
+
+    elif args.subcommands == "cd":
+        change_directory(args.test, configuration=configuration)
+
+    elif args.subcommands == "docs":
+        webbrowser.open("https://buildtest.readthedocs.io/")
+
+    elif args.subcommands == "info":
+        buildtest_info(configuration, system)
+
+    elif args.subcommands in ["debug", "debugreport"]:
+        print_debug_report(system, configuration)
+
+    elif args.subcommands in ["unittests", "test"]:
+        run_unit_tests(
+            pytestopts=args.pytestopts,
+            sourcefiles=args.sourcefiles,
+            enable_coverage=args.coverage,
+        )
+
+    elif args.subcommands == "tutorial-examples":
+        generate_tutorial_examples(
+            examples=args.examples,
+            dryrun=args.dryrun,
+            write=args.write,
+            failfast=args.failfast,
+        )
+
+    elif args.subcommands in ["stylecheck", "style"]:
+        run_style_checks(
+            no_black=args.no_black,
+            no_isort=args.no_isort,
+            no_pyflakes=args.no_pyflakes,
+            apply_stylechecks=args.apply,
+        )
+
+    elif args.subcommands in ["commands", "cmds"]:
+        list_buildtest_commands(with_aliases=args.with_aliases)
+
+
+def setup(args):
+    """This function sets up the environment for buildtest.
+    This function initializes logging, configuration, and system object.
+    This function returns a tuple of system, configuration, buildtest_editor, and report_file.
+    This function is called at the beginning of main() function.
+
+    Args:
+        args (argparse.Namespace): An argparse namespace object containing command line arguments
+
+    Returns:
+        Tuple: A tuple of system, configuration, buildtest_editor, and report_file
+    """
+    install(show_locals=True)
+    no_color = False
+
+    # disable color if buildtest --no-color or BUILDTEST_COLOR=False is set
+    if args.no_color or os.getenv("BUILDTEST_COLOR") == "False":
+        no_color = True
+
+    console.no_color = no_color
+
+    if args.verbose:
+        console.print("Coloring mode: ", "off" if no_color else "on")
+
+    report_file = args.report
 
     if is_file(BUILDTEST_LOGFILE):
         remove_file(BUILDTEST_LOGFILE)
@@ -156,7 +257,39 @@ def main():
 
         BuildspecCache(roots=root_buildspecs, configuration=configuration)
 
-    # buildtest build command
+    return system, configuration, buildtest_editor, report_file
+
+
+def handle_log_viewing(args):
+    """This function handles the viewing of the log file. If the user specifies --view-log or --print-log, this function will display the log file content and return True.
+    If the log file is not present, this function will raise an exception. If the user does not specify --view-log or --print-log, this function will return False.
+
+    Args:
+        args (argparse.Namespace): An argparse namespace object containing command line arguments
+
+    Returns:
+        bool: Return True if user specifies --view-log or --print-log and implements logic, otherwise return False
+    """
+    if args.view_log or args.print_log:
+        # if logfile is not present we should raise exception since this file is only created upon running 'buildtest build'
+        if not is_file(BUILDTEST_LOGFILE):
+            raise BuildTestError(
+                f"Unable to find logfile: {BUILDTEST_LOGFILE}, please run 'buildtest build'"
+            )
+
+        content = read_file(BUILDTEST_LOGFILE)
+
+        if args.view_log:
+            with console.pager():
+                console.print(content)
+        else:
+            console.print(content)
+
+        return True
+    return False
+
+
+def handle_build_command(args, configuration, report_file):
     if args.subcommands in ["build", "bd"]:
         stdout_file = tempfile.NamedTemporaryFile(delete=True, suffix=".txt")
         with Tee(stdout_file.name):
@@ -206,199 +339,115 @@ def main():
 
         stdout_file.close()
 
-    # buildtest build history
-    if args.subcommands in ["history", "hy"]:
-        build_history(args)
 
-    # implementation for 'buildtest buildspec find'
-    elif args.subcommands in ["buildspec", "bc"]:
-        if args.buildspecs_subcommand in ["find", "f"]:
-            buildspec_find(args=args, configuration=configuration)
-        elif args.buildspecs_subcommand in ["summary", "sm"]:
-            summarize_buildspec_cache(
-                pager=args.pager, configuration=configuration, color=args.color
-            )
-        elif args.buildspecs_subcommand in ["show", "s"]:
-            show_buildspecs(
-                test_names=args.name, configuration=configuration, theme=args.theme
-            )
-        elif args.buildspecs_subcommand in ["show-fail", "sf"]:
-            show_failed_buildspecs(
-                configuration=configuration,
-                test_names=args.name,
-                report_file=report_file,
-                theme=args.theme,
-            )
-        elif args.buildspecs_subcommand in ["edit-test", "et"]:
-            edit_buildspec_test(
-                test_names=args.name,
-                configuration=configuration,
-                editor=buildtest_editor,
-            )
-        elif args.buildspecs_subcommand in ["edit-file", "ef"]:
-            edit_buildspec_file(
-                buildspecs=args.file,
-                configuration=configuration,
-                editor=buildtest_editor,
-            )
-        elif args.buildspecs_subcommand in ["maintainers", "m"]:
-            name = None
-            if hasattr(args, "name"):
-                name = args.name
-            buildspec_maintainers(
-                configuration=configuration,
-                breakdown=args.breakdown,
-                terse=args.terse,
-                header=args.no_header,
-                color=args.color,
-                name=name,
-                row_count=args.row_count,
-            )
+def handle_buildspec_command(args, configuration, report_file, buildtest_editor):
+    if args.buildspecs_subcommand in ["find", "f"]:
+        buildspec_find(args=args, configuration=configuration)
+    elif args.buildspecs_subcommand in ["summary", "sm"]:
+        summarize_buildspec_cache(
+            pager=args.pager, configuration=configuration, color=args.color
+        )
+    elif args.buildspecs_subcommand in ["show", "s"]:
+        show_buildspecs(
+            test_names=args.name, configuration=configuration, theme=args.theme
+        )
+    elif args.buildspecs_subcommand in ["show-fail", "sf"]:
+        show_failed_buildspecs(
+            configuration=configuration,
+            test_names=args.name,
+            report_file=report_file,
+            theme=args.theme,
+        )
+    elif args.buildspecs_subcommand in ["edit-test", "et"]:
+        edit_buildspec_test(
+            test_names=args.name, configuration=configuration, editor=buildtest_editor
+        )
+    elif args.buildspecs_subcommand in ["edit-file", "ef"]:
+        edit_buildspec_file(
+            buildspecs=args.file, configuration=configuration, editor=buildtest_editor
+        )
+    elif args.buildspecs_subcommand in ["maintainers", "m"]:
+        name = None
+        if hasattr(args, "name"):
+            name = args.name
+        buildspec_maintainers(
+            configuration=configuration,
+            breakdown=args.breakdown,
+            terse=args.terse,
+            header=args.no_header,
+            color=args.color,
+            name=name,
+            row_count=args.row_count,
+        )
 
-        elif args.buildspecs_subcommand in ["validate", "val"]:
-            buildspec_validate(
-                buildspecs=args.buildspec,
-                excluded_buildspecs=args.exclude,
-                tags=args.tag,
-                executors=args.executor,
-                configuration=configuration,
-            )
+    elif args.buildspecs_subcommand in ["validate", "val"]:
+        buildspec_validate(
+            buildspecs=args.buildspec,
+            excluded_buildspecs=args.exclude,
+            tags=args.tag,
+            executors=args.executor,
+            configuration=configuration,
+        )
 
-    # running buildtest inspect
-    elif args.subcommands in ["inspect", "it"]:
-        report = Report(configuration=configuration, report_file=report_file)
-        if args.inspect in ["list", "ls"]:
-            inspect_list(
-                report,
-                terse=args.terse,
-                no_header=args.no_header,
-                builder=args.builder,
-                color=args.color,
-                pager=args.pager,
-                row_count=args.row_count,
-            )
-        # implements command 'buildtest inspect name'
-        if args.inspect in ["name", "n"]:
-            inspect_by_name(report, names=args.name, pager=args.pager)
 
-        if args.inspect in ["query", "q"]:
-            inspect_query(
-                report,
-                name=args.name,
-                theme=args.theme,
-                output=args.output,
-                error=args.error,
-                testpath=args.testpath,
-                buildscript=args.buildscript,
-                buildenv=args.buildenv,
-                pager=args.pager,
-            )
+def handle_inspect_command(args, configuration, report_file):
+    report = Report(configuration=configuration, report_file=report_file)
+    if args.inspect in ["list", "ls"]:
+        inspect_list(
+            report,
+            terse=args.terse,
+            no_header=args.no_header,
+            builder=args.builder,
+            color=args.color,
+            pager=args.pager,
+            row_count=args.row_count,
+        )
+    # implements command 'buildtest inspect name'
+    if args.inspect in ["name", "n"]:
+        inspect_by_name(report, names=args.name, pager=args.pager)
 
-        if args.inspect in ["buildspec", "b"]:
-            inspect_buildspec(
-                report,
-                input_buildspecs=args.buildspec,
-                all_records=args.all,
-                pager=args.pager,
-            )
-        return
-
-    elif args.subcommands in ["stats"]:
-        stats_cmd(name=args.name, configuration=configuration, report_file=report_file)
-    # running buildtest config
-    elif args.subcommands in ["config", "cg"]:
-        #  running buildtest config compilers
-        if args.config in ["compilers", "co"]:
-            compiler_cmd(args, configuration)
-        else:
-            config_cmd(command_args=args, configuration=configuration, editor=buildtest_editor, system=system)
-
-    # buildtest report
-    elif args.subcommands in ["report", "rt"]:
-        report_cmd(args, configuration=configuration, report_file=report_file)
-
-    elif args.subcommands == "path":
-        path_cmd(
+    if args.inspect in ["query", "q"]:
+        inspect_query(
+            report,
             name=args.name,
-            outfile=args.outfile,
-            errfile=args.errfile,
+            theme=args.theme,
+            output=args.output,
+            error=args.error,
             testpath=args.testpath,
             buildscript=args.buildscript,
-            stagedir=args.stagedir,
             buildenv=args.buildenv,
+            pager=args.pager,
+        )
+
+    if args.inspect in ["buildspec", "b"]:
+        inspect_buildspec(
+            report,
+            input_buildspecs=args.buildspec,
+            all_records=args.all,
+            pager=args.pager,
+        )
+
+
+def handle_cdash_command(args, configuration, report_file):
+    cdash_config = deep_get(configuration.target_config, "cdash")
+
+    if not cdash_config:
+        raise BuildTestError(
+            f"We found no 'cdash' setting set in configuration file: {configuration.file}. Please specify 'cdash' setting in order to use 'buildtest cdash' command"
+        )
+
+    if args.cdash == "view":
+        view_cdash_project(
+            cdash_config=cdash_config, config_file=configuration.file, open_browser=True
+        )
+    elif args.cdash == "upload":
+        upload_test_cdash(
+            build_name=args.buildname,
             configuration=configuration,
+            site=args.site,
             report_file=report_file,
+            open_browser=args.open,
         )
-    # running bnuildtest schema
-    elif args.subcommands == "schema":
-        schema_cmd(args)
-
-    # running buildtest cdash
-    elif args.subcommands == "cdash":
-        cdash_config = deep_get(configuration.target_config, "cdash")
-
-        if not cdash_config:
-            raise BuildTestError(
-                f"We found no 'cdash' setting set in configuration file: {configuration.file}. Please specify 'cdash' setting in order to use 'buildtest cdash' command"
-            )
-
-        if args.cdash == "view":
-            view_cdash_project(
-                cdash_config=cdash_config,
-                config_file=configuration.file,
-                open_browser=True,
-            )
-        elif args.cdash == "upload":
-            upload_test_cdash(
-                build_name=args.buildname,
-                configuration=configuration,
-                site=args.site,
-                report_file=report_file,
-                open_browser=args.open,
-            )
-
-    elif args.subcommands in ["show", "s"]:
-        buildtest_show(command=args.command)
-
-    elif args.subcommands == "clean":
-        clean(configuration=configuration, yes=args.yes)
-
-    elif args.subcommands == "cd":
-        change_directory(args.test, configuration=configuration)
-
-    elif args.subcommands == "docs":
-        webbrowser.open("https://buildtest.readthedocs.io/")
-
-    elif args.subcommands == "info":
-        buildtest_info(configuration, system)
-    elif args.subcommands in ["debug", "debugreport"]:
-        print_debug_report(system, configuration)
-
-    elif args.subcommands in ["unittests", "test"]:
-        run_unit_tests(
-            pytestopts=args.pytestopts,
-            sourcefiles=args.sourcefiles,
-            enable_coverage=args.coverage,
-        )
-
-    elif args.subcommands == "tutorial-examples":
-        generate_tutorial_examples(
-            examples=args.examples,
-            dryrun=args.dryrun,
-            write=args.write,
-            failfast=args.failfast,
-        )
-
-    elif args.subcommands in ["stylecheck", "style"]:
-        run_style_checks(
-            no_black=args.no_black,
-            no_isort=args.no_isort,
-            no_pyflakes=args.no_pyflakes,
-            apply_stylechecks=args.apply,
-        )
-
-    elif args.subcommands in ["commands", "cmds"]:
-        list_buildtest_commands(with_aliases=args.with_aliases)
 
 
 if __name__ == "__main__":
