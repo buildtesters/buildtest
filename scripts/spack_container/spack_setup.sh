0feb6194b462f1a3fd5531b58531445d9f6555b3
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2026-09-21 18:34:17 -0400

    Update scripts/spack_container/spack_setup.sh for SC25 Spack tutorial container

--- scripts/spack_container/spack_setup.sh
+++ scripts/spack_container/spack_setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 . /home/spack/spack/share/spack/setup-env.sh
 . "$(spack location -i lmod)/lmod/lmod/init/bash"
-spack module tcl refresh --delete-tree -y
 module use /home/spack/modules/linux-ubuntu22.04-x86_64_v3
-
