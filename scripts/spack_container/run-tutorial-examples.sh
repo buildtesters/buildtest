bcb7524d2b4f1e3ce8c2576e4592dffebd065022
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2026-09-22 09:03:44 -0400

    Update tutorial example container reference

--- scripts/spack_container/run-tutorial-examples.sh
+++ scripts/spack_container/run-tutorial-examples.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# This script can be run as follows: docker run -v $BUILDTEST_ROOT:/home/spack/buildtest ghcr.io/buildtesters/buildtest_spack:latest bash /home/spack/buildtest/scripts/spack_container/run-tutorial-examples.sh
+# This script can be run as follows: docker run -v $BUILDTEST_ROOT:/home/spack/buildtest ghcr.io/buildtesters/buildtest_spack:spack-sc25 bash /home/spack/buildtest/scripts/spack_container/run-tutorial-examples.sh
 source /etc/profile
 source /home/spack/buildtest/scripts/spack_container/setup.sh
 python /home/spack/buildtest/scripts/spack_container/doc-examples.py
