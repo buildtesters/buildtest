3128f0ae10a9591eb1f78206c5e13ee752050b9d
Shahzeb Siddiqui <shahzebmsiddiqui@gmail.com>
2026-09-21 21:54:05 -0400

    Initialize SPACK_ROOT in container shell

--- scripts/spack_container/spack_setup.sh
+++ scripts/spack_container/spack_setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-. /home/spack/spack/share/spack/setup-env.sh
+export SPACK_ROOT=/home/spack/spack
+export PATH="${SPACK_ROOT}/bin:${PATH}"
+
+. "${SPACK_ROOT}/share/spack/setup-env.sh"
 . "$(spack location -i lmod)/lmod/lmod/init/bash"
 module use /home/spack/modules/linux-ubuntu22.04-x86_64_v3
